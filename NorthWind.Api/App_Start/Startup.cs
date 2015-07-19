0798e016cb79d5e4ebdbc6dd33748fd9f8cf77f7
wayne <world.wayno@gmail.com>
2015-07-19 16:41:05 -0400

    adding assembly nuget packages

---
+++ NorthWind.Api/App_Start/Startup.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(NorthWind.Api.Startup))]
+
+namespace NorthWind.Api
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var config = new HttpConfiguration();
+
+            //app.UseWebApi(config);
+
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ NorthWind.Api/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NorthWind.Api.MvcApplication" Language="C#" %>
